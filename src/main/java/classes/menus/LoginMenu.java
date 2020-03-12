fcc5646e21f00feeffc4cc9418cdbf7aa98c6cf6
Ozukyo <ozukyo@gmail.com>
2020-03-12 22:17:13 +0100

    Refactor displayMenu(), split into three methods

--- src/main/java/classes/menus/LoginMenu.java
+++ src/main/java/classes/menus/LoginMenu.java
@@ -5,11 +5,8 @@ import classes.controllers.SqlController;
 import classes.controllers.UserController;
 import classes.enums.Option;
 import classes.enums.Role;
-import classes.users.Admin;
 import classes.users.Customer;
-import classes.users.User;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoginMenu {
@@ -28,7 +25,7 @@ public class LoginMenu {
     public void displayLoginMenu() throws Exception {
         boolean isRunning = true;
         System.out.println("Welcome to the Online Shop!");
-        while(isRunning) {
+        while (isRunning) {
             System.out.println("""
                     (1) Log in
                     (2) Create new user
@@ -45,53 +42,14 @@ public class LoginMenu {
                     String password = "";
                     String email = "";
 
-                    boolean flag = true;
-                    while (flag) {
-                        login = input.getStringInputWithMessage("Enter login: ");
-                        if (userController.getIsLoginTaken(login)) {
-                            System.out.println("Login is already taken");
-                        }
-                        else {
-                            flag = false;
-                        }
-                    }
-
-                    flag = true;
-                    while (flag) {
-                        String regex = "^[a-zA-Z0-9]{8,16}";
-                        Pattern pattern = Pattern.compile(regex);
-
-                        boolean isPasswordChoosen = false;
-                        while(!isPasswordChoosen){
-                            System.out.println("Password must contain at least 8 max 16 characters.");
-                            password = input.getStringInputWithMessage("Enter password: ");
-                            // Matcher matcher = pattern.matcher(password);
-                            if (password.matches(regex)) {
-                                isPasswordChoosen = true;
-                                flag = false;
-
-                            }
-                            else {
-                                System.out.println("Incorrect password");
-                            }
-                        }
-                    }
-
-                    flag = true;
-                    while (flag) {
-                        email = input.getStringInputWithMessage("Enter email: ");
-                        if (userController.getIsEmailTaken(email)) {
-                            System.out.println("Email is already taken");
-                        }
-                        else {
-                            flag = false;
-                        }
-                    }
+                    login = getCustomerLogin(userController, login);
+                    password = getCustomerPassword(password);
+                    email = getCustomerEmail(userController, email);
 
                     try {
                         userController.addUser(new Customer(login, password, email, Role.CUSTOMER));
-                    } catch (Exception e){
-                        throw new Exception ("Something went wrong");
+                    } catch (Exception e) {
+                        throw new Exception("Something went wrong");
                     }
                     userController.viewUsersTable();
 
@@ -103,5 +61,52 @@ public class LoginMenu {
             }
         }
 
+    }
+
+    private String getCustomerEmail(UserController userController, String email) {
+        boolean flag;
+
+        flag = true;
+        while (flag) {
+            email = input.getStringInputWithMessage("Enter email: ");
+            if (userController.getIsEmailTaken(email)) {
+                System.out.println("Email is already taken");
+            } else {
+                flag = false;
+            }
+        }
+        return email;
+    }
+
+    private String getCustomerPassword(String password) {
+        String regex = "^[a-zA-Z0-9]{8,16}";
+        Pattern pattern = Pattern.compile(regex);
+
+        boolean isPasswordChoosen = false;
+        while (!isPasswordChoosen) {
+            System.out.println("Password must contain at least 8 max 16 characters.");
+            password = input.getStringInputWithMessage("Enter password: ");
+            // Matcher matcher = pattern.matcher(password);
+            if (password.matches(regex)) {
+                isPasswordChoosen = true;
+
+            } else {
+                System.out.println("Incorrect password");
+            }
+        }
+        return password;
+    }
+
+    private String getCustomerLogin(UserController userController, String login) {
+        boolean flag = true;
+        while (flag) {
+            login = input.getStringInputWithMessage("Enter login: ");
+            if (userController.getIsLoginTaken(login)) {
+                System.out.println("Login is already taken");
+            } else {
+                flag = false;
+            }
+        }
+        return login;
     }
 }
