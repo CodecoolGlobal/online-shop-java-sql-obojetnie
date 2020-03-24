0c9256ae6abefdf6c59c5c6f33e860ecf9a0db01
Ozukyo <ozukyo@gmail.com>
2020-03-24 13:08:54 +0100

    Add update

--- src/main/java/classes/controllers/ProductController.java
+++ src/main/java/classes/controllers/ProductController.java
@@ -154,6 +154,7 @@ public class ProductController {
         }
     }
 
+
     public Product getProductFromDatabase(String name) {
         String SELECT_SQL = "SELECT * FROM products WHERE name = '" + name + "';";
         try {
--- src/main/java/classes/inputs/InputTaker.java
+++ src/main/java/classes/inputs/InputTaker.java
@@ -2,10 +2,11 @@ package classes.inputs;
 
 import classes.enums.Option;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class InputTaker {
-  
+
     Scanner scanner = new Scanner(System.in);
 
     public int getIntInput() {
@@ -23,7 +24,8 @@ public class InputTaker {
 
     public double getDoubleInputWithMessage(String message) {
         System.out.println(message);
-        return scanner.nextDouble();
+        Scanner doubleScanner = new Scanner(System.in).useLocale(Locale.US);
+        return doubleScanner.nextDouble();
     }
 
     public String getStringInputWithMessage(String message) {
@@ -32,6 +34,26 @@ public class InputTaker {
     }
 
     public Option getOptionInt() throws Exception {
+        int input = scanner.nextInt();
+        Option option;
+        switch (input) {
+            case 1 -> option = Option.ONE;
+            case 2 -> option = Option.TWO;
+            case 3 -> option = Option.THREE;
+            case 4 -> option = Option.FOUR;
+            case 5 -> option = Option.FIVE;
+            case 6 -> option = Option.SIX;
+            case 7 -> option = Option.SEVEN;
+            case 8 -> option = Option.EIGHT;
+            case 9 -> option = Option.NINE;
+            case 0 -> option = Option.ZERO;
+            default -> throw new Exception("Something went wrong.");
+        }
+        return option;
+    }
+
+    public Option getOptionIntWithMessage(String message) throws Exception {
+        System.out.println(message);
         int input = scanner.nextInt();
         Option option;
         switch (input) {
